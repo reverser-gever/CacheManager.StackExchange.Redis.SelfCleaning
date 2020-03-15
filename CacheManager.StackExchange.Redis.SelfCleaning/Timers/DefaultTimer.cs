bbadcf1097cadd43c3317a7a0aa0a365ca9a7320
IdanZel <idan.zel.harari@gmail.com>
2020-03-15 19:18:59 +0200

    Created ITimer and implemented DefaultTimer

---
+++ CacheManager.StackExchange.Redis.SelfCleaning/Timers/DefaultTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Timers;
+
+namespace CacheManager.StackExchange.Redis.SelfCleaning.Timers
+{
+    public class DefaultTimer : ITimer
+    {
+        private readonly Timer _timer;
+        
+        public event Action Elapsed;
+
+        public DefaultTimer(double interval)
+        {
+            _timer = new Timer(interval);
+        }
+        
+        public void Start()
+        {
+            _timer.Elapsed += OnElapsed;
+            _timer.Start();
+        }
+
+        public void Dispose()
+        {
+            _timer.Elapsed -= OnElapsed;
+            _timer.Dispose();
+        }
+
+        private void OnElapsed(object sender, ElapsedEventArgs args)
+        {
+            Elapsed?.Invoke();
+        }
+    }
+}
