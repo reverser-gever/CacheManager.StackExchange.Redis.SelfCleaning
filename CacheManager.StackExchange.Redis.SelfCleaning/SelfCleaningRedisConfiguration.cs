c1d9eb1d1a347fd77344fb7caf433074c3377932
IdanZel <idan.zel.harari@gmail.com>
2020-03-15 19:19:26 +0200

    Added SelfCleaningRedisConfiguration

---
+++ CacheManager.StackExchange.Redis.SelfCleaning/SelfCleaningRedisConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CacheManager.Redis;
+using CacheManager.StackExchange.Redis.SelfCleaning.Timers;
+using StackExchange.Redis;
+
+namespace CacheManager.StackExchange.Redis.SelfCleaning
+{
+    public class SelfCleaningRedisConfiguration : RedisConfiguration
+    {
+        public IDatabase RedisDatabase { get; }
+
+        public ITimer CleanupTimer { get; }
+        
+        public TimeSpan SlidingExpiration { get; }
+
+        public SelfCleaningRedisConfiguration(IDatabase redisDatabase, ITimer cleanupTimer, TimeSpan slidingExpiration)
+        {
+            RedisDatabase = redisDatabase;
+            CleanupTimer = cleanupTimer;
+            SlidingExpiration = slidingExpiration;
+        }
+    }
+}
