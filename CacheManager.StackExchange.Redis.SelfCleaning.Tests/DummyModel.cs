461f418a14c01af9427e3b4bb8fcd8fefd416c41
IdanZel <idan.zel.harari@gmail.com>
2020-03-24 19:39:47 +0200

    Created DummyModel

---
+++ CacheManager.StackExchange.Redis.SelfCleaning.Tests/DummyModel.cs
@@ -0,0 +1,21 @@
+﻿using ProtoBuf;
+
+namespace CacheManager.StackExchange.Redis.SelfCleaning.Tests
+{
+    [ProtoContract]
+    public class DummyModel
+    {
+        [ProtoMember(1)]
+        public string Property { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && obj is DummyModel other && Property.Equals(other.Property);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Property != null ? Property.GetHashCode() : 0);
+        }
+    }
+}
