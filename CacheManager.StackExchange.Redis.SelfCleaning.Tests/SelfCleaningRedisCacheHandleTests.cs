a9ab507bdb70aeeb516aa6a07754f6c9290d2907
Kurtz <omerkurtz@gmail.com>
2020-03-31 15:54:07 +0300

    Made some progress

--- CacheManager.StackExchange.Redis.SelfCleaning.Tests/SelfCleaningRedisCacheHandleTests.cs
+++ CacheManager.StackExchange.Redis.SelfCleaning.Tests/SelfCleaningRedisCacheHandleTests.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using CacheManager.Core;
+using CacheManager.Core.Internal;
+using CacheManager.Core.Logging;
 using CacheManager.StackExchange.Redis.SelfCleaning.Timers;
 using Moq;
 using NUnit.Framework;
@@ -13,33 +16,28 @@ namespace CacheManager.StackExchange.Redis.SelfCleaning.Tests
     public class SelfCleaningRedisCacheHandleTests
     {
         private Mock<IServer> _serverMock;
-        
+
         private Mock<IConnectionMultiplexer> _connectionMock;
         private Mock<ITimer> _timerMock;
         private TimeSpan _timeToLive;
 
-        private ICacheManager<DummyModel> _cache;
-        private SelfCleaningRedisCacheHandle<DummyModel> _cacheHandle;
+        private TestableSelfCleaningRedisCacheHandle<DummyModel> _cacheHandle;
 
         [SetUp]
         public void Setup()
         {
             _serverMock = new Mock<IServer>();
-            
+
             _connectionMock = new Mock<IConnectionMultiplexer>();
             _timerMock = new Mock<ITimer>();
             _timeToLive = TimeSpan.FromSeconds(1);
 
             SetupConnectionMock();
 
-            _cache = CacheFactory.Build<DummyModel>(part => part
-                .WithSerializer(typeof(DummySerializer))
-                .WithSelfCleaningRedisConfiguration(_connectionMock.Object, _timerMock.Object, _timeToLive,
-                    out string configurationKey)
-                .WithSelfCleaningRedisCacheHandle(configurationKey));
-
-            _cacheHandle = (SelfCleaningRedisCacheHandle<DummyModel>) _cache.CacheHandles.Single();
+            _cacheHandle = TestUtilities.CreateSelfCleaningRedisCacheHandleForTests(
+                _connectionMock.Object, _timerMock.Object, _timeToLive);
         }
+
 
         private void SetupConnectionMock()
         {
@@ -57,27 +55,66 @@ namespace CacheManager.StackExchange.Redis.SelfCleaning.Tests
         {
             // Arrange
             _timerMock.SetupAdd(timer => timer.Elapsed += It.IsAny<Action>());
-            
+
             // Act
             _cacheHandle.Start();
-            
+
             // Assert
             _timerMock.VerifyAdd(timer => timer.Elapsed += It.IsAny<Action>(), Times.Once);
             _timerMock.Verify(timer => timer.Start(), Times.Once);
         }
 
-        [Test]
-        public void Dispose_HappyFlow_UnsubscribesFromElapsedAndDisposesTimer()
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Dispose_HappyFlow_UnsubscribesFromElapsedAndDisposesTimerRegardlessOfBlooeanArgument(bool disposeManaged)
         {
             // Arrange
             _timerMock.SetupRemove(timer => timer.Elapsed -= It.IsAny<Action>());
-            
+
             // Act
-            _cache.Dispose();
-            
+            _cacheHandle.RunDispose(disposeManaged);
+
             // Assert
             _timerMock.VerifyRemove(timer => timer.Elapsed -= It.IsAny<Action>(), Times.Once);
             _timerMock.Verify(timer => timer.Dispose(), Times.Once);
         }
+
+        [Test]
+        public void RunCleanup_HappyFlowRedisDataBaseIsntFoundInServers_EventWasntRaised()
+        {
+            // Arrange
+            _serverMock.Setup(server => server.Keys(It.IsAny<int>(),
+                    It.IsAny<RedisValue>(), It.IsAny<int>(), It.IsAny<long>(),
+                    It.IsAny<int>(), It.IsAny<CommandFlags>()))
+                .Returns(new List<RedisKey>());
+
+            // Act
+            _cacheHandle.Start();
+            _timerMock.Raise(timer => timer.Elapsed += null);
+
+            // Assert
+            _serverMock.VerifyNoOtherCalls();
+        }
+
+
     }
+
+    #region Testable SelfCleaningRedisCacheHandle internal class
+
+    /// <summary>
+    /// Exposes the protected <see cref="SelfCleaningRedisCacheHandle.Dispose"/>
+    /// </summary>
+    internal class TestableSelfCleaningRedisCacheHandle<TCacheValue> : SelfCleaningRedisCacheHandle<TCacheValue>
+    {
+        public TestableSelfCleaningRedisCacheHandle(ICacheManagerConfiguration managerConfiguration,
+            CacheHandleConfiguration configuration, ILoggerFactory loggerFactory, ICacheSerializer serializer)
+            : base(managerConfiguration, configuration, loggerFactory, serializer) { }
+
+        public void RunDispose(bool disposeManaged)
+        {
+            base.Dispose(disposeManaged);
+        }
+    }
+
+    #endregion
 }
---
+++ CacheManager.StackExchange.Redis.SelfCleaning.Tests/TestUtilities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using CacheManager.Core;
+using CacheManager.Core.Internal;
+using CacheManager.Core.Logging;
+using CacheManager.StackExchange.Redis.SelfCleaning.Timers;
+using StackExchange.Redis;
+
+namespace CacheManager.StackExchange.Redis.SelfCleaning.Tests
+{
+    internal static class TestUtilities
+    {
+        #region Testable SelfCleaningRedisCacheHandle class Creation
+        internal static TestableSelfCleaningRedisCacheHandle<DummyModel>CreateSelfCleaningRedisCacheHandleForTests(
+            IConnectionMultiplexer connectionMock, ITimer timerMock, TimeSpan timeToLive)
+        {
+            var cache = CacheFactory.Build<DummyModel>(part => part
+                .WithSerializer(typeof(DummySerializer))
+                .WithSelfCleaningRedisConfiguration(connectionMock, timerMock, timeToLive,
+                    out string configurationKey)
+                .WithTestableSelfCleaningRedisCacheHandle(configurationKey));
+
+            return (TestableSelfCleaningRedisCacheHandle<DummyModel>)cache.CacheHandles.Single();
+        }
+
+        private static ConfigurationBuilderCacheHandlePart WithTestableSelfCleaningRedisCacheHandle(
+            this ConfigurationBuilderCachePart part, string configurationKey)
+        {
+            // This method tells the CacheFactory to create an instance of SelfCleaningRedisCacheHandle when it builds
+            // the cache (CacheManager does this using reflection). This instance will use the configuration added by
+            // the WithSelfCleaningRedisConfiguration method
+            return part.WithHandle(typeof(TestableSelfCleaningRedisCacheHandle<>), configurationKey);
+        }
+
+        #endregion
+    }
+}
